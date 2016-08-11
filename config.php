0c02912ddcf397a3b1bbe9695e41632a4708eee2
SFE-14881\administrador <shingoo_n@yahoo.com.mx>
2016-08-11 00:00:15 -0500

    v 0.0.1

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+if($_SERVER["SERVER_NAME"]=="localhost"){
+	
+	define("DATA_BASE_HOST","localhost");
+	define("DATA_BASE_NAME","pentaglobal");
+	define("DATA_BASE_USER","");
+	define("DATA_BASE_PWD","");
+	
+}
---
+++ index.php
@@ -0,0 +1,1 @@
+aqui va el tablero de resumen
