9af098b94c4e8e46efb8132c8dffd4c06043fb08
giovanni <giovanni@giovanni-HP>
2016-08-15 21:54:15 -0500

    cambiar imagen login

--- login/index.php
+++ login/index.php
@@ -4,7 +4,7 @@
 
     <div class="middle-box text-center animated fadeInDown">
         <div style="background-color: #FFF; padding: 30px 50px 30px 50px;">
-            <img src="<?php echo $raizProy?>img/logo_globmint2.png" class="img-responsive" />
+            <img src="<?php echo $raizProy?>img/logo_globmint2.png" class="img-responsive" style='margin-left: auto; margin-right: auto' />
 
             <div class="clear">&nbsp;</div>
             <div class="clear">&nbsp;</div>
