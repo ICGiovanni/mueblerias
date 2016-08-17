724dd387eee48cd652f0aea9269b42f6edf3362d
SFE-14881\administrador <shingoo_n@yahoo.com.mx>
2016-08-16 21:35:57 -0500

    no message

--- gastos/index.php
+++ gastos/index.php
@@ -14,10 +14,15 @@ $objGasto = new Gasto();
                     <div class="ibox-title">
                         <h5>Gastos</h5>
                         <div class="ibox-tools">
+							
+                            <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#myModal">+ Nuevo Gasto</button>
+                            <!--<a class="collapse-link">
+                                <i class="fa fa-plus-square-o"></i>
+                            </a>-->
                             <a class="collapse-link">
                                 <i class="fa fa-chevron-up"></i>
                             </a>
-                           
+							
                         </div>
                     </div>
                     <div class="ibox-content">
@@ -280,167 +285,7 @@ $objGasto = new Gasto();
                         <td class="center">-</td>
                         <td class="center">A</td>
                     </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera 7.5</td>
-                        <td>Win 95+ / OSX.2+</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera 8.0</td>
-                        <td>Win 95+ / OSX.2+</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera 8.5</td>
-                        <td>Win 95+ / OSX.2+</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera 9.0</td>
-                        <td>Win 95+ / OSX.3+</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera 9.2</td>
-                        <td>Win 88+ / OSX.3+</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera 9.5</td>
-                        <td>Win 88+ / OSX.3+</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Opera for Wii</td>
-                        <td>Wii</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Nokia N800</td>
-                        <td>N800</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Presto</td>
-                        <td>Nintendo DS browser</td>
-                        <td>Nintendo DS</td>
-                        <td class="center">8.5</td>
-                        <td class="center">C/A<sup>1</sup></td>
-                    </tr>
-                    <tr class="gradeC">
-                        <td>KHTML</td>
-                        <td>Konqureror 3.1</td>
-                        <td>KDE 3.1</td>
-                        <td class="center">3.1</td>
-                        <td class="center">C</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>KHTML</td>
-                        <td>Konqureror 3.3</td>
-                        <td>KDE 3.3</td>
-                        <td class="center">3.3</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>KHTML</td>
-                        <td>Konqureror 3.5</td>
-                        <td>KDE 3.5</td>
-                        <td class="center">3.5</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeX">
-                        <td>Tasman</td>
-                        <td>Internet Explorer 4.5</td>
-                        <td>Mac OS 8-9</td>
-                        <td class="center">-</td>
-                        <td class="center">X</td>
-                    </tr>
-                    <tr class="gradeC">
-                        <td>Tasman</td>
-                        <td>Internet Explorer 5.1</td>
-                        <td>Mac OS 7.6-9</td>
-                        <td class="center">1</td>
-                        <td class="center">C</td>
-                    </tr>
-                    <tr class="gradeC">
-                        <td>Tasman</td>
-                        <td>Internet Explorer 5.2</td>
-                        <td>Mac OS 8-X</td>
-                        <td class="center">1</td>
-                        <td class="center">C</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Misc</td>
-                        <td>NetFront 3.1</td>
-                        <td>Embedded devices</td>
-                        <td class="center">-</td>
-                        <td class="center">C</td>
-                    </tr>
-                    <tr class="gradeA">
-                        <td>Misc</td>
-                        <td>NetFront 3.4</td>
-                        <td>Embedded devices</td>
-                        <td class="center">-</td>
-                        <td class="center">A</td>
-                    </tr>
-                    <tr class="gradeX">
-                        <td>Misc</td>
-                        <td>Dillo 0.8</td>
-                        <td>Embedded devices</td>
-                        <td class="center">-</td>
-                        <td class="center">X</td>
-                    </tr>
-                    <tr class="gradeX">
-                        <td>Misc</td>
-                        <td>Links</td>
-                        <td>Text only</td>
-                        <td class="center">-</td>
-                        <td class="center">X</td>
-                    </tr>
-                    <tr class="gradeX">
-                        <td>Misc</td>
-                        <td>Lynx</td>
-                        <td>Text only</td>
-                        <td class="center">-</td>
-                        <td class="center">X</td>
-                    </tr>
-                    <tr class="gradeC">
-                        <td>Misc</td>
-                        <td>IE Mobile</td>
-                        <td>Windows Mobile 6</td>
-                        <td class="center">-</td>
-                        <td class="center">C</td>
-                    </tr>
-                    <tr class="gradeC">
-                        <td>Misc</td>
-                        <td>PSP browser</td>
-                        <td>PSP</td>
-                        <td class="center">-</td>
-                        <td class="center">C</td>
-                    </tr>
-                    <tr class="gradeU">
-                        <td>Other browsers</td>
-                        <td>All others</td>
-                        <td>-</td>
-                        <td class="center">-</td>
-                        <td class="center">U</td>
-                    </tr>
+                   
                     </tbody>
                     <tfoot>
                     <tr>
@@ -468,27 +313,27 @@ $objGasto = new Gasto();
 
         </div>
         </div>
-
-
-
+		
     <!-- Mainly scripts -->
-    <script src="<?=FINAL_URL?>js/jquery-2.1.1.js"></script>
-    <script src="<?=FINAL_URL?>js/bootstrap.min.js"></script>
-    <script src="<?=FINAL_URL?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="<?=FINAL_URL?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="<?=FINAL_URL?>js/plugins/jeditable/jquery.jeditable.js"></script>
+    <script src="<?=$raizProy?>js/jquery-2.1.1.js"></script>
+    <script src="<?=$raizProy?>js/bootstrap.min.js"></script>
+    <script src="<?=$raizProy?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?=$raizProy?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="<?=$raizProy?>js/plugins/jeditable/jquery.jeditable.js"></script>
 
-    <script src="<?=FINAL_URL?>js/plugins/dataTables/datatables.min.js"></script>
+    <script src="<?=$raizProy?>js/plugins/dataTables/datatables.min.js"></script>
 
     <!-- Custom and plugin javascript -->
-    <script src="<?=FINAL_URL?>js/inspinia.js"></script>
-    <script src="<?=FINAL_URL?>js/plugins/pace/pace.min.js"></script>
+    <script src="<?=$raizProy?>js/inspinia.js"></script>
+    <script src="<?=$raizProy?>js/plugins/pace/pace.min.js"></script>
 
     <!-- Page-Level Scripts -->
     <script>
         $(document).ready(function(){
             $('.dataTables-example').DataTable({
 				searching: false,
+				ordering:  false,
+				paging: false,
                 dom: '<"html5buttons"B>lTfgitp',
                 buttons: [
                     { extend: 'copy'},
@@ -512,6 +357,89 @@ $objGasto = new Gasto();
         });
         
     </script>
+
+	
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Nuevo Gasto</h4>
+      </div>
+      <div class="modal-body">
+	  <table>
+			<tr>
+				<td>No de documento:</td>
+				<td><input type="text" name="gasto_no_documento" id="gasto_no_documento" /></td>
+			</tr>
+			<tr>
+				<td>Fecha de vencimiento:</td>
+				<td><input type="text" name="gasto_fecha_vencimiento" id="gasto_fecha_vencimiento" /></td>
+			</tr>
+			<tr>
+				<td>Programar Recordatorio:</td>
+				<td><input type="checkbox" name="gasto_fecha_recordatorio_si" id="gasto_fecha_recordatorio_si" /></td>
+			</tr>
+			<tr>
+				<td>Fecha de recordatorio:</td>
+				<td><input type="text" name="gasto_fecha_recordatorio" id="gasto_fecha_recordatorio" /></td>
+			</tr>
+			<tr>
+				<td>Categoria:</td>
+				<td><input type="text" name="gasto_categoria_id" id="gasto_categoria_id" /></td>
+			</tr>
+			<tr>
+				<td>Concepto:</td>
+				<td><input type="text" name="gasto_concepto" id="gasto_concepto" value=""></td>
+			</tr>
+			<tr>
+				<td>Descripción:</td>
+				<td><input type="text" name="gasto_descripcion" id="gasto_descripcion" value=""></td>
+			</tr>
+			<tr>
+				<td>Monto:</td>
+				<td><input type="text" name="gasto_monto" id="gasto_monto" value=""></td>
+			</tr>
+	  </table>
+		 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+        <button id="boton_crea_gasto" type="button" class="btn btn-primary" onclick="crea_gasto();">Guardar</button>
+		<span id="span_crea_gasto"></span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+function crea_gasto(){
+	$("#boton_crea_gasto").addClass("disabled");
+	$("#span_crea_gasto").addClass("glyphicon glyphicon-refresh glyphicon-refresh-animate");
+	
+	gasto_no_documento=$("#gasto_no_documento").val();
+	gasto_fecha_vencimiento=$("#gasto_fecha_vencimiento").val();
+	gasto_fecha_recordatorio_si=$("#gasto_fecha_recordatorio_si").val();
+	gasto_fecha_recordatorio=$("#gasto_fecha_recordatorio" ).val();
+	gasto_categoria_id=$("#gasto_categoria_id" ).val();
+	gasto_concepto=$("#gasto_concepto").val();		
+	gasto_descripcion=$("#gasto_descripcion").val();
+	gasto_monto=$("#gasto_monto").val();
+	gasto_status_id = '1';
+	
+	$.ajax({
+		type: "POST",
+		url: "ajax/crea_gasto.php",			
+		data: {gasto_no_documento:gasto_no_documento, gasto_fecha_vencimiento:gasto_fecha_vencimiento, gasto_fecha_recordatorio_si:gasto_fecha_recordatorio_si ,gasto_fecha_recordatorio:gasto_fecha_recordatorio ,gasto_categoria_id: gasto_categoria_id, gasto_concepto: gasto_concepto, gasto_descripcion: gasto_descripcion, gasto_monto: gasto_monto, gasto_status_id:gasto_status_id},
+		success: function(msg){
+			
+			$("#myModal").modal('hide');
+			$("#boton_crea_gasto").removeClass().addClass("btn btn-primary");
+			$("#span_crea_gasto").removeClass();
+		}		
+	});
+}
+</script>
 
 </body>
 
