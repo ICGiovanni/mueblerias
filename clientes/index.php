bcbc09994fdd94e842e8afc046524c846d2f1cfc
Spawn <umedina86@yahoo.com.mx>
2016-08-23 00:40:03 -0500

    Form

--- clientes/index.php
+++ clientes/index.php
@@ -10,16 +10,16 @@
             <h2>Clientes</h2>
             <ol class="breadcrumb">
                 <li>
-                    <a href="">This is</a>
+                    <a href="">Clientes</a>
                 </li>
                 <li class="active">
-                    <strong>Clientes</strong>
+                    <strong>Lista de Clientes</strong>
                 </li>
             </ol>
         </div>
         <div class="col-sm-8">
             <div class="title-action">
-                <a href="" class="btn btn-primary">Agregar un nuevo Cliente</a>
+                <a href="./nuevo_cliente.php" class="btn btn-primary">Agregar un nuevo Cliente</a>
             </div>
         </div>
     </div>
---
+++ clientes/nuevo_cliente.php
@@ -0,0 +1,71 @@
+<?php
+    include $_SERVER['REDIRECT_PATH_CONFIG'].'/config.php';
+    include $pathProy.'/header.php';
+    include $pathProy.'/menu.php';
+?>
+
+
+    <div class="row wrapper border-bottom white-bg page-heading">
+        <div class="col-sm-4">
+            <h2>Clientes</h2>
+            <ol class="breadcrumb">
+                <li>
+                    <a href="">Clientes</a>
+                </li>
+                <li class="active">
+                    <strong>Nuevo Cliente</strong>
+                </li>
+            </ol>
+        </div>
+        <div class="col-sm-8">
+            <div class="title-action">
+                <a href="./index.php" class="btn btn-primary">Lista de Clientes</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper wrapper-content animated fadeInRight">
+		<form method="get" class="form-horizontal">
+			<div class="form-group"><label class="col-sm-2 control-label">Normal</label>
+			<div class="col-sm-6"><input type="text" class="form-control"></div>
+            </div>
+		</form>
+	</div>
+
+
+
+    <!-- Page-Level Scripts -->
+    <script>
+        $(document).ready(function(){
+            $('.dataTables-example').DataTable({
+                dom: '<"html5buttons"B>lTfgitp',
+                buttons: [
+                    { extend: 'copy'},
+                    {extend: 'csv'},
+                    {extend: 'excel', title: 'ExampleFile'},
+                    {extend: 'pdf', title: 'ExampleFile'},
+
+                    {extend: 'print',
+                     customize: function (win){
+                            $(win.document.body).addClass('white-bg');
+                            $(win.document.body).css('font-size', '10px');
+
+                            $(win.document.body).find('table')
+                                    .addClass('compact')
+                                    .css('font-size', 'inherit');
+                    }
+                    }
+                ]
+
+            });
+
+
+
+        });
+
+    </script>
+
+
+<?php
+    include $pathProy.'footer.php';
+?>
