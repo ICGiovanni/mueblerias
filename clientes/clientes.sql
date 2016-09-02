472aeb02dc127c649ee26db345e11af5dbacc90c
uriel.medina <uriel.medina>
2016-09-02 11:08:39 -0500

    SQL Clientes

---
+++ clientes/clientes.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14.8
+-- http://www.phpmyadmin.net
+--
+-- Servidor: db642823290.db.1and1.com
+-- Tiempo de generación: 02-09-2016 a las 11:07:28
+-- Versión del servidor: 5.5.50-0+deb7u2-log
+-- Versión de PHP: 5.4.45-0+deb7u4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `db642823290`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `apellidoP` varchar(100) NOT NULL,
+  `apellidoM` varchar(100) NOT NULL,
+  `razon_social` varchar(100) NOT NULL,
+  `rfc` varchar(13) NOT NULL,
+  `calle` varchar(100) NOT NULL,
+  `num_exterior` varchar(50) NOT NULL,
+  `num_interior` varchar(50) NOT NULL,
+  `colonia` varchar(100) NOT NULL,
+  `codigo_postal` varchar(10) NOT NULL,
+  `municipio` varchar(100) NOT NULL,
+  `id_estado` int(11) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `telefono_alterno` varchar(50) NOT NULL,
+  `celular` varchar(100) NOT NULL,
+  `celularA` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `emailA` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
