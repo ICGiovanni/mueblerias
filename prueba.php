b5bfb28fa867ab0822c87fbf050e3e1ce9f3a0a5
uriel.medina <uriel.medina>
2016-09-23 18:00:42 -0500

    Productos

--- prueba.php
+++ prueba.php
@@ -1,10 +1,7 @@
-<?php
+<?php 
+
 include $_SERVER['REDIRECT_PATH_CONFIG'].'/config.php';
-require_once($_SERVER["REDIRECT_PATH_CONFIG"].'/models/general/class.General.php');
-$general=new General();
 
-$date=date('Y-m-d H:i:s');
-
-echo $general->getDate($date);
+echo FINAL_URL;
 
 ?>
