71bce9be85fa809cb3e7abf130055d9fc5181c06
Edgar <shingoo_n@yahoo.com.mx>
2017-06-06 09:50:10 -0500

    Se agrega validacion de no avance en envio y facturacion hasta que eligan opcion SI o NO

--- clientes/ajax_set_cliente_sin_factura_data.php
+++ clientes/ajax_set_cliente_sin_factura_data.php
@@ -2,9 +2,8 @@
 session_start();
 
 if(isset( $_SESSION["punto_venta"] )){
-	if(isset( $_SESSION["punto_venta"]["facturacion"]  )){
-		unset( $_SESSION["punto_venta"]["facturacion"] );
-	}
+	$_SESSION["punto_venta"]["facturacion"] = array();
+	$_SESSION["punto_venta"]["facturacion"]["cliente_direccion_id"] = 0;
 }
 include $_SERVER['REDIRECT_PATH_CONFIG'].'/config.php';
 require_once($_SERVER["REDIRECT_PATH_CONFIG"].'clientes/models/class.Clientes.php');
